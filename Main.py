8550933687a4305c79d2a090e93c1d904f91296c
czw <3502318983@qq.com>
2019-04-01 13:32:50 +0800

    首次提交代码

---
+++ Main.py
@@ -0,0 +1,172 @@
+import time
+import re
+
+hasDayLoop = False
+info = None
+loopTime = 0
+
+class Infomation:
+    def __init__(self, fromLog = True):
+        if fromLog:
+            with open("tmp.log", "r", encoding="utf-8") as file:
+                for each in file:
+                    if each.startswith("#") or each.find("=") < 0:
+                        continue
+                    tmp = each.replace(" ", "").replace("\n", "")
+                    tmp = tmp.split("=")
+                    setattr(self, tmp[0], int(tmp[1]))
+        else:
+            nowDate = time.localtime(time.time())
+            self.year = nowDate.tm_year
+            self.month = nowDate.tm_mon
+            self.day = nowDate.tm_mday
+            self.week_day = nowDate.tm_wday
+            self.monthUse = 0
+            self.dayUse = 0
+            self.yearUse = 0
+
+
+    def writeToLog(self):
+        file = open("tmp.log", "r", encoding="utf-8")
+        writeContent = ''
+        attrs = dir(self)
+        for each in file:
+            for i in attrs:
+                if i in each:
+                    each = re.sub("\\b\d+\\b", str(getattr(self, i)), each)
+            writeContent += each
+        file.close()
+        file = open("tmp.log", "w", encoding="utf-8")
+        file.write(writeContent)
+        file.close()
+
+    def equal(self, otherInfo):
+        comparation = ("year", "month", "day")
+        for each in comparation:
+            if getattr(self, each) != getattr(otherInfo, each):
+                return False
+        return True
+
+    def refresh(self, nowDate):
+        refresh_attributes = ["day", "month", "year"]
+
+        def refreshByAttributes(refresh_attributes):
+            changed = []
+            for each in refresh_attributes:
+                if getattr(self, each) != getattr(nowDate, each):
+                    setattr(self, each, getattr(nowDate, each))
+                    changed.append(each)
+            return changed
+
+        changed = refreshByAttributes(refresh_attributes)
+        refresh_attributes.clear()
+        if len(changed) != 0:
+            self.restTime = self.autoExitTime
+        for each in changed:
+            refresh_attributes.append(each + 'Use')
+
+        refreshByAttributes(refresh_attributes)
+
+    def refreshRestTime(self):
+        #该方法用于设置关机倒计时
+        # 将关机倒计时设置为“当前时间”到“自动关机时间”的间隔
+        nowTime = time.localtime(time.time())
+        hour = nowTime.tm_hour
+        minute = nowTime.tm_min
+        restTime = (self.closeTimeHour - hour) * 60
+        restTime += self.closeTimeMinute - minute
+        #如果当日最长开机时间小于关机倒计时，则将关机倒计时设置为当日最长开机时间。
+        if self.autoExitTime < restTime:
+            restTime = self.autoExitTime;
+        elif restTime < 0:
+            restTime = 0
+        if restTime < self.restTime:
+            self.restTime = restTime
+
+    def timeAppend(self, min):
+        self.dayUse += min
+        self.monthUse += min
+        self.yearUse += min
+        self.totleUse += min
+
+class Exit:
+    def exit(self):
+        pass
+
+def dayLoop():
+    """
+    获取当前文件记录，并写入info中
+    如果当前记录的日期与今天的日期一致，则不做改变
+    如果当前记录的日期与今天的日期不一致，则改变
+
+    在软件打开或者到了新的一天后调用。
+    """
+    global info
+    if info == None:
+        info = Infomation(fromLog=True)
+    nowDate = Infomation(fromLog=False)
+    if info.equal(nowDate):
+        pass
+    else:
+        info.refresh(nowDate)
+
+    global hasDayLoop
+    if not hasDayLoop:
+        info.refreshRestTime()
+    hasDayLoop = True
+
+def fiveMinLoop():
+    """
+    该循环5分钟循环一次，获取当前时间，如和
+    """
+    global info
+    info.refreshRestTime()
+    nowDate = time.localtime(time.time())
+    if nowDate.tm_mday != info.day:
+        dayLoop()
+    info.writeToLog()
+
+def oneMinLoop():
+    """
+    该循环每分钟循环一次，用于关机
+    """
+    global loopTime
+    loopTime += 1
+    if loopTime >= 5:
+        fiveMinLoop()
+        loopTime = 0
+
+    global info
+    if info.restTime <= 0:
+        #写入记录
+        fiveMinLoop()
+        if info.restTime > 0:
+            info.restTime -= 1;
+            info.timeAppend(1)
+            return
+        #打开关机倒计时
+        import webbrowser
+        webbrowser.open("http://127.0.0.1:8888/CountDown.html")
+        time.sleep(15)
+        #关机
+        import os
+        if os.name.find('nt') >= 0:
+            os.system("shutdown -s -t 0")
+        else:
+            os.system("shutdown now")
+        return
+    else:
+        info.restTime -= 1;
+    info.timeAppend(1)
+
+def start():
+    dayLoop()
+    import webbrowser
+    webbrowser.open("http://127.0.0.1:8888/index.html")
+    while(True):
+        time.sleep(60)
+        oneMinLoop()
+
+if __name__ == "__main__":
+    start()
+    pass
---
+++ Server.py
@@ -0,0 +1,87 @@
+from  http.server import HTTPServer,BaseHTTPRequestHandler
+from urllib.parse import urlparse
+from threading import Thread
+import Main
+
+class  MServer(BaseHTTPRequestHandler):
+    types = {".html": "text/html", '.png': 'image/png', '.jpg':'image/jpeg', ".log": "text/plain"}
+    #get
+    def do_GET(self):
+        path = urlparse(self.path)
+        types = self.types
+        contentType = types['.log']
+        if path.path.find(".") < 0:
+            self.handleQuery(path)
+            return
+        for each in types:
+            if path.path.endswith(each):
+                contentType = types[each]
+                break
+
+        content = None
+        try:
+            with open("." + path.path, "rb") as file:
+                content = file.read()
+            self.send_response(200)
+        except:
+            self.send_error(404, "File not Exists")
+            return
+        self.send_header('Content-type', contentType)
+        self.end_headers()
+        self.wfile.write(content)
+
+    def handleQuery(self, path):
+        tmp = path.query
+        tmp = tmp.split("&")
+        query = {}
+        for each in tmp:
+            each = each.split("=")
+            query[each[0]] = each[1]
+
+        content = bytes(getattr(QueryHandler, query['m'])(query), 'utf-8')
+
+        self.send_response(200)
+        self.send_header("Content-type", self.types[".log"])
+        self.end_headers()
+        self.wfile.write(content)
+
+class QueryHandler:
+    @staticmethod
+    def setRestTime(query):
+        Main.info.restTime = int(query['restTime'])
+        return 'ok'
+
+    @staticmethod
+    def setAutoExitTime(query):
+        Main.info.autoExitTime = int(query['autoExitTime'])
+        return 'ok'
+
+    @staticmethod
+    def getRestTime(query):
+        return str(Main.info.restTime)
+
+    @staticmethod
+    def setCloseTime(query):
+        Main.info.closeTimeHour = int(query['hour'])
+        Main.info.closeTimeMinute = int(query['minute'])
+        return 'ok'
+
+    @staticmethod
+    def getLog(query):
+        tmp = ""
+        contents = ('totleUse', 'yearUse', 'dayUse', 'monthUse', 'autoExitTime')
+        for each in contents:
+            tmp += each + ' = ' + str(getattr(Main.info, each)) + '\n'
+
+        tmp += 'closeTime = ' + str(Main.info.closeTimeHour * 60 + Main.info.closeTimeMinute) + '\n'
+
+        return tmp
+
+
+if __name__ == "__main__":
+    loopthread = Thread(target=Main.start)
+    loopthread.start()
+    port = 8888
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, MServer)
+    httpd.serve_forever()
