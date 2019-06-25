51dad8ff4efbb5964c08af588574b6535161a702
czw <3502318983@qq.com>
2019-06-25 21:59:53 +0800

    把var改为了let

--- js/search.js
+++ js/search.js
@@ -7,8 +7,8 @@ class Search{
             this.engine = 'https://www.baidu.com';
             localStorage.setItem('search-engine', this.engine);
         }else{
-            var eles = document.querySelectorAll("#search-option option");
-            for(var i = 0; i < eles.length; i++){
+            let eles = document.querySelectorAll("#search-option option");
+            for(let i = 0; i < eles.length; i++){
                 if(eles[i].getAttribute("data-url") == this.engine){
                     eles[i].parentNode.selectedIndex = i;
                     break;
@@ -18,31 +18,31 @@ class Search{
         //绑定事件
         document.querySelector("#search-option").onchange = (evt) => {
             const ele = evt.target || evt.srcElement;
-            var index = ele.selectedIndex;
-            var url = ele.querySelectorAll("option")[index].getAttribute('data-url');
+            let index = ele.selectedIndex;
+            let url = ele.querySelectorAll("option")[index].getAttribute('data-url');
             this.engine = url;
             localStorage.setItem('search-engine', this.engine);
         };
         document.querySelector(".search-sure").onclick = (evt) => {
-            var ele = evt.target || evt.srcElement;
+            let ele = evt.target || evt.srcElement;
             ele = ele.previousSibling;
             while(ele.nodeType != 1)
                 ele = ele.previousSibling;
-            var text = ele.value;
+            let text = ele.value;
             ele.value = "";
             this.startSearch(text);
         };
         document.querySelector(".search-input").onkeydown = (evt) =>{
             if(evt.keyCode == 13){
-                var ele = evt.target || evt.srcElement;
-                var text = ele.value;
+                let ele = evt.target || evt.srcElement;
+                let text = ele.value;
                 ele.value = '';
                 this.startSearch(text);
             }
         };
     }
     startSearch(text){
-        var url = this.engine;
+        let url = this.engine;
         if(/www.baidu.com/.test(this.engine)){
             url += '/s?wd=' + text;
         }else if(/www.bing.com/.test(this.engine)){
