bcd8fff3389aeba75bff11ac65d20585a18c06cd
YandaChen <3502318983@qq.com>
2020-12-13 19:41:39 +0800

    创建了新的分支，该分支不再强制关机，只定时提醒休息

--- Server.py
+++ Server.py
@@ -11,9 +11,7 @@ class  MServer(BaseHTTPRequestHandler):
         path = urlparse(self.path)
         types = self.types
         contentType = types['.log']
-        if path.path.find(".") < 0:
-            self.handleQuery(path)
-            return
+
         for each in types:
             if path.path.endswith(each):
                 contentType = types[each]
@@ -31,54 +29,6 @@ class  MServer(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(content)
         self.wfile.close()
-
-    def handleQuery(self, path):
-        tmp = path.query
-        tmp = tmp.split("&")
-        query = {}
-        for each in tmp:
-            each = each.split("=")
-            query[each[0]] = each[1]
-
-        content = bytes(getattr(QueryHandler, query['m'])(query), 'utf-8')
-
-        self.send_response(200)
-        self.send_header("Content-type", self.types[".log"])
-        self.end_headers()
-        self.wfile.write(content)
-        self.wfile.close()
-
-class QueryHandler:
-    @staticmethod
-    def setRestTime(query):
-        Main.info.restTime = int(query['restTime'])
-        return 'ok'
-
-    @staticmethod
-    def setAutoExitTime(query):
-        Main.info.autoExitTime = int(query['autoExitTime'])
-        return 'ok'
-
-    @staticmethod
-    def getRestTime(query):
-        return str(Main.info.restTime)
-
-    @staticmethod
-    def setCloseTime(query):
-        Main.info.closeTimeHour = int(query['hour'])
-        Main.info.closeTimeMinute = int(query['minute'])
-        return 'ok'
-
-    @staticmethod
-    def getLog(query):
-        tmp = ""
-        contents = ('totleUse', 'yearUse', 'dayUse', 'monthUse', 'autoExitTime')
-        for each in contents:
-            tmp += each + ' = ' + str(getattr(Main.info, each)) + '\n'
-
-        tmp += 'closeTime = ' + str(Main.info.closeTimeHour * 60 + Main.info.closeTimeMinute) + '\n'
-
-        return tmp
 
 
 if __name__ == "__main__":
